0ca7cf16f3fc3c8023917305525526edb20de065
Han Ga Eun <gdd72582@gmail.com>
2024-02-17 16:40:34 +0900

    1541_java_한가은

---
+++ 5주차/BOJ_1541.java
@@ -0,0 +1,26 @@
+package BOJ_5th_Week;
+
+import java.util.Scanner;
+
+public class BOJ_1541 {
+    static int answer = 0;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String e = sc.nextLine();
+        String[] str = e.split("-");
+        for(int i = 0; i < str.length; i++){
+            int temp = mySum(str[i]);
+            if(i == 0) answer += temp;
+            else answer -= temp;
+        }
+        System.out.println(answer);
+    }
+    static int mySum (String a){
+        int sum = 0;
+        String temp[] = a.split("[+]");
+        for(int i = 0; i < temp.length; i++){
+            sum += Integer.parseInt(temp[i]);
+        }
+        return sum;
+    }
+}
